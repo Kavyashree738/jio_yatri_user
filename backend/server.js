14565bd8d1612dbfd215cfc449c3d0644e2ca751
Kavyashree738 <jioyattri@gmail.com>
2025-06-14 06:45:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const partnerRoutes = require('./routes/partnerRoutes');
 const enterpriseRoutes = require('./routes/enterpriseRoutes');
 const shipmentRoutes = require('./routes/shipmentRoutes');
+const addressRoutes=require('./routes/addressRoutes')
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
@@ -47,6 +48,7 @@ app.use(express.json());
 app.use('/api', partnerRoutes);
 app.use('/api', enterpriseRoutes);
 app.use('/api/shipments', shipmentRoutes);
+app.use('/api/address', addressRoutes); 
 // app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => {
