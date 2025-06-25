0f03600356072627dfb2d2c8be1dd6b7d21c893d
Kavyashree738 <jioyattri@gmail.com>
2025-06-25 22:09:19 +0530

    Update UserShipments.js

--- src/components/UserShipments.js
+++ src/components/UserShipments.js
@@ -164,7 +164,7 @@ const UserShipments = () => {
       setLoading(true);
       setError('');
 
-      const response = await axios.get('http://localhost:5000/api/shipments/my-shipments', {
+      const response = await axios.get('https://jio-yatri-user.onrender.com/api/shipments/my-shipments', {
         headers: { Authorization: `Bearer ${token}` },
       });
 
