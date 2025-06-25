1f70f07441af8661d40cba131cdce12420b62beb
Kavyashree738 <jioyattri@gmail.com>
2025-06-25 16:36:35 +0200

    added routes

---
+++ backend/routes/driverRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { registerDriver,checkDriverExists, getDriver, updateDriverStatus,getDriverStatus ,updateDriverLocation,getDriverLocation,getAvailableShipments,registerFCMToken} = require('../controllers/driverController');
+const verifyFirebaseToken = require('../middleware/verifyFirebaseToken');
+
+router.get('/check/:userId', verifyFirebaseToken, checkDriverExists);
+router.post('/register', verifyFirebaseToken, registerDriver);
+router.get('/info/:userId', verifyFirebaseToken, getDriver);
+router.get('/status', verifyFirebaseToken, getDriverStatus);
+router.put('/status', verifyFirebaseToken, updateDriverStatus);
+
+// Location endpoints
+router.get('/location', verifyFirebaseToken, getDriverLocation);
+router.put('/location', verifyFirebaseToken, updateDriverLocation);
+
+// backend/routes/driverRoutes.js
+router.post('/fcm-token', verifyFirebaseToken, registerFCMToken);
+
+
+module.exports = router;
