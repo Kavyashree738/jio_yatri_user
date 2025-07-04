3d74a2d2b4858b9fca4f0597129f881790603730
Kavyashree738 <jioyattri@gmail.com>
2025-07-04 06:50:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Handle preflight CORS
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
