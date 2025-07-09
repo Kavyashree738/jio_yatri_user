7b7b14c2c52b5958629aed52b5b9970e5f72f1b8
Kavyashree738 <jioyattri@gmail.com>
2025-07-09 14:30:53 +0530

    Update addressRoutes.js

--- backend/routes/addressRoutes.js
+++ backend/routes/addressRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const addressController = require('../controllers/addressController');
 
-// Route: GET /api/address/autocomplete
+
 router.get('/autocomplete', addressController.autocomplete);
 
-// Route: GET /api/address/geocode
+
 router.get('/geocode', addressController.geocode);
 router.get('/reverse-geocode', addressController.reverseGeocode);
 
