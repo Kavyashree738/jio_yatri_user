948e449262858f5a3a1ea6c8f1838463ea75f5cd
Kavyashree738 <jioyattri@gmail.com>
2025-07-09 10:36:04 +0530

    Update SplashScreen.js

--- src/components/pages/SplashScreen.js
+++ src/components/pages/SplashScreen.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import '../../styles/SplashScreen.css';
 import logo from '../../assets/images/logo.jpg';
-import ownerImage from '../../assets/images/hero-image.jpg'; // Make sure to add your owner image
+import ownerImage from '../../assets/images/owner-image.jpeg'
 
 const SplashScreen = () => {
   const navigate = useNavigate();
@@ -18,7 +18,7 @@ const SplashScreen = () => {
 
   return (
     <div className="splash-container">
-      <motion.div 
+      <motion.div
         className="splash-content"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -30,19 +30,19 @@ const SplashScreen = () => {
           className="logo-container"
           initial={{ scale: 0.5 }}
           animate={{ scale: 1 }}
-          transition={{ 
+          transition={{
             type: 'spring',
             stiffness: 100,
             damping: 10
           }}
         >
-          <img 
-            src={logo} 
-            alt="Jio Yatri Logo" 
+          <img
+            src={logo}
+            alt="Jio Yatri Logo"
             className="logo-image"
           />
         </motion.div>
-        
+
         {/* Owner image with nice styling */}
         <motion.div
           className="owner-container"
@@ -50,26 +50,40 @@ const SplashScreen = () => {
           animate={{ y: 0, opacity: 1 }}
           transition={{ delay: 0.3 }}
         >
-          <img 
-            src={ownerImage} 
-            alt="Owner" 
+          <img
+            src={ownerImage}
+            alt="Owner"
             className="owner-image"
           />
         </motion.div>
-        
+
         <motion.h1
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
-          transition={{ 
+          transition={{
             delay: 0.6,
             type: 'spring',
             stiffness: 100
           }}
           className="welcome-text"
         >
-          Welcome to Jio Yatri
+          <motion.span
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1.1, opacity: 1 }}
+            transition={{
+              delay: 0.7,
+              type: 'spring',
+              stiffness: 120,
+              damping: 8,
+              repeat: 1,
+              repeatType: 'reverse'
+            }}
+            className="highlight-word"
+          >
+            Welcome
+          </motion.span>
+          &nbsp;to Jio Yatri
         </motion.h1>
-        
         <motion.p
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
@@ -78,7 +92,7 @@ const SplashScreen = () => {
         >
           Your trusted delivery partner
         </motion.p>
-        
+
         {/* Loading indicator */}
         <motion.div
           initial={{ opacity: 0 }}
