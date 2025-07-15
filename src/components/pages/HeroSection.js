409134ed17ce68d0c01ff4e6b222522fd142e120
Kavyashree738 <jioyattri@gmail.com>
2025-07-15 12:46:16 +0530

    Update HeroSection.js

--- src/components/pages/HeroSection.js
+++ src/components/pages/HeroSection.js
@@ -164,7 +164,8 @@ const HeroSection = () => {
   if (isWebView()) {
     // Redirect to a page that handles Google Redirect login
     const loginUrl = `${window.location.origin}/google-login`; // This should be a Route in your React app
-     window.AndroidInterface?.openInBrowser(loginUrl);
+    window.location.href = loginUrl;
+     window.open(loginUrl, '_blank'); 
     return;
   }
 
