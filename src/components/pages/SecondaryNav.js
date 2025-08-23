e8d04ce6cf73cf0ce3a9fcd61ab239deff83b1d1
Kavyashree738 <jioyattri@gmail.com>
2025-08-23 22:42:15 +0530

    added the image for secondary nav

--- src/components/pages/SecondaryNav.js
+++ src/components/pages/SecondaryNav.js
@@ -3,15 +3,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import hotelIcon from '../../assets/images/header/hotel.jpg';
+import hotelIcon from '../../assets/images/header/hotel-food.jpg';
 import groceryIcon from '../../assets/images/header/grocery.jpg';
 import vegetableIcon from '../../assets/images/header/vegetables.jpg';
 import provisionIcon from '../../assets/images/header/provision.jpg';
 import medicalIcon from '../../assets/images/header/medical.jpg';
+import hotel from '../../assets/images/header/hotel.jpg'
 import '../../styles/SecondaryNav.css';
 
 const secondaryNavItems = [
   { name: 'Hotels', path: '/shops/hotel', icon: hotelIcon },
+  { name: 'Hotels', path: '/shops/hotel-book', icon: hotel },
   { name: 'Groceries', path: '/shops/grocery', icon: groceryIcon },
   { name: 'Vegetables', path: '/shops/vegetable', icon: vegetableIcon },
   { name: 'Provisions', path: '/shops/provision', icon: provisionIcon },
