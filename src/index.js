72fd599e25ec93ba3d7b0e44c3ca53aed20ed372
Kavyashree738 <jioyattri@gmail.com>
2025-09-05 11:20:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import App from './App';
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+window.addEventListener('push', (e) => {
+  const { data, fromTap } = e.detail || {};
+  console.log('📩 Bridged push into WebView:', data, 'fromTap:', fromTap);
+
+  if (data?.shipmentId) {
+    // Example deep link
+    window.history.pushState({}, '', `/orders/${data.shipmentId}`);
+  }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
