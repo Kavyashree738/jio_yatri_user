eaf1a2b2ba90e24ec5e75b5e2bd70347856bb523
Kavyashree738 <jioyattri@gmail.com>
2025-10-16 17:19:37 +0530

    Update notificationService.js

--- backend/services/notificationService.js
+++ backend/services/notificationService.js
@@ -24,11 +24,7 @@ const sendToToken = async (token, { title, body, data = {} }) => {
         body,
         icon: '/logo.jpg',
       },
-     android: {
-      priority: 'high',
-      // Optional: uncomment if you want to force the same channel as Dart
-      // notification: { channelId: 'driver_general' },
-    },
+
    
       fcmOptions: {
         link: '/business-orders', // where you want to land when user clicks
